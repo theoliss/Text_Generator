2914da13cc4984ecf6af68ab89c3dc28d958a072
theoliss <theo.lissarrague@gmail.com>
2023-12-12 18:05:04 +0100

    IT IS WORKING (but need minor fixes)

--- scriptJS.js
+++ scriptJS.js
@@ -1,5 +1,5 @@
 // Define the character set used in your model training
-const chars = ['%', '$', ' ', 'i', 'f', '&', '#', 'e', '8', 'w', 'g', 'o', "'", '2', '[', 's', 'q', '!', 'k', 'b', 'h', 'z', 'l', '1', '+', '3', '?', '~', ')', 't', '5', 'c', ':', 'n', 'm', 'p', '(', '/', 'j', ']', 'y', 'a', ',', 'u', '"', '0', '6', '7', '_', '@', '{', '`', '4', 'x', '^', '-', 'r', 'd', '9', 'v', '\\', '\n', '}']; // Add or remove characters as needed
+const chars = ['\n', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '+', ',', '-', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '?', '@', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '}', '~']; // Add or remove characters as needed
 const stoi = {};
 chars.forEach((char, index) => stoi[char] = index);
 const itos = chars;
@@ -20,6 +20,7 @@ function softmax(arr)
     const total = scaled.reduce((acc, val) => acc + val, 0);
     return scaled.map(val => val / total);
 }
+
 function sampleIndex(probabilities) {
     const rnd = Math.random();
     let cumSum = 0;
@@ -30,24 +31,27 @@ function sampleIndex(probabilities) {
     return probabilities.length - 1;
 }
 
-function decodeLogits(logits) {
-    const probabilities = softmax(logits);
-    const index = sampleIndex(probabilities);
-    return itos[index];
-}
-
 function decode(outputIndices) {
-    // Assuming each position in outputIndices corresponds to a set of logits for a character
     let text = '';
 
     for (let i = 0; i < outputIndices.length; i += vocabSize) {
         const logits = outputIndices.slice(i, i + vocabSize);
         const probabilities = softmax(logits);
-        const maxIndex = probabilities.indexOf(Math.max(...probabilities));
-        text += itos[maxIndex];
+        const randomIndex = weightedRandomSelect(probabilities);
+        text += itos[randomIndex];
     }
 
-    return text;
+    return text[text.length-1];
+}
+
+function weightedRandomSelect(probabilities) {
+    let sum = 0;
+    const r = Math.random();
+    for (let i = 0; i < probabilities.length; i++) {
+        sum += probabilities[i];
+        if (r <= sum) return i;
+    }
+    return probabilities.length - 1; // Return the last index in case random number exceeds sum
 }
 
 
@@ -59,65 +63,20 @@ async function loadModel() {
     session = await ort.InferenceSession.create("./model_files/recipes.onnx");
 }
 
-// Function to generate text based on the user input
-async function generateText() {
+async function generateNextChar(context) {
+    const encodedInput = encode(context);
+    if (encodedInput.length === 0) {console.error('Encoded input is empty');return;}
+    const inputTensor = new ort.Tensor("int32", new Int32Array(encodedInput), [1, encodedInput.length]);
+    const outputMap = await session.run({'inputs': inputTensor });
+    const outputTensor = outputMap['outputs'];
+    const output = outputTensor.data;
+    const predicted_char = decode(output);
 
-    if (!session) {
-        console.error('Model not loaded yet');
-        return;
-    }
-
-    const encodedInput = encode("test");
-
-    if (encodedInput.length === 0) {
-        console.error('Encoded input is empty');
-        return;
-    }
-
-    // Preprocess encodedInput to match the shape [64, 256]
-    // This might involve padding or truncating the input
-    const paddedInput = new Array(sequence_length).fill(0);  // Filling with zeros (or any appropriate padding value)
-    for (let i = 0; i < Math.min(encodedInput.length, sequence_length); i++) {
-        paddedInput[i] = encodedInput[i];
-    }
-
-    // Create the input tensor with the correct shape
-    const inputTensor = new ort.Tensor("int32", new Int32Array(paddedInput), [1, sequence_length]);
-    // Run the model
-    try {
-        const feeds = { 'inputs': inputTensor };
-        const outputMap = await session.run(feeds);
-        const outputTensor = outputMap['outputs'];
-        const outputIndices = Array.from(outputTensor.data);
-        const generatedText = decode(outputIndices);
-        document.getElementById('generated_text').innerHTML = generatedText;
-   
-    } 
-    catch (error) {
-        console.error('Error during model run:', error);
-    }
+    return predicted_char;
 }
 
 // Load the model immediately when the script is loaded
 loadModel();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var word_to_add = " a word";
-var generate = true;
 
 var displayed_texte = document.getElementById('generated_text');
 var display_recipe_name = document.getElementById('display_recipe_name');
@@ -125,29 +84,31 @@ var generate_button = document.getElementById('generate_btn');
 var reset_button = document.getElementById('reset_btn');
 var recipe_name = document.getElementById('input_recipe_name_text_box');
 
-
-function add_text()
+generate_button.addEventListener('click', async function() 
 {
-    if(generate)
-        {
-            displayed_texte.innerHTML = displayed_texte.innerHTML + '\n' + word_to_add ;
-        }
-}
-
-
-generate_button.addEventListener('click', function() 
-{
-    if (recipe_name.value == ""){
-        display_recipe_name.innerHTML = "fully generated recipe :";
+    let context = recipe_name.value;
+    
+    if (context == ""){
+        context = "fully generated recipe";
     }
-    else {
-        display_recipe_name.innerHTML = recipe_name.value + " :";
-    }
-    generate = true;
+    context += " :\n";
+    let text_to_show = context;
+
     document.getElementById("input_paragraph").hidden = true;
     document.getElementById("generate_btn").hidden = true;
 
-    generateText();
+    for(let i = 0; i < 1000; i++)
+    {
+        let memo_char = await generateNextChar(context);
+        text_to_show += memo_char;
+        if(context.length >= 126)
+        {
+            context.slice(1);
+        }
+        context += memo_char;
+        document.getElementById('generated_text').innerText = text_to_show
+        await new Promise(resolve => setTimeout(resolve,10));
+    }
 });
 
 
@@ -158,5 +119,4 @@ reset_button.addEventListener('click', function()
         recipe_name.value = "";
         document.getElementById("input_paragraph").hidden = false;
         document.getElementById("generate_btn").hidden = false;
-        generate = false;
 });
--- testing_the_model_in_local.py
+++ testing_the_model_in_local.py
@@ -14,13 +14,26 @@ itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
+#_________________________test of the pytorch model_______________________________________________
+
 model = GPTLanguageModel().to('cuda')
 model.load_state_dict(torch.load('./model_files/recipes.pt'))
 model.eval()
-# user_input = input("from what do you want to start ? ")
+user_input = input("from what do you want to start ? ")
 
-# context = torch.tensor([encode(user_input)], dtype=torch.long, device='cuda')
-# print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
+context = torch.tensor([encode(user_input)], dtype=torch.int32, device='cuda')
+print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
+
+#_________________________export the model in onnx _______________________________________________
 
 dummy_input = torch.zeros(1, 128, dtype=torch.int32).to("cuda")
-torch.onnx.export(model, dummy_input, "./model_files/recipes.onnx",input_names=['inputs'], output_names=['outputs'], export_params=True, opset_version=10)
+torch.onnx.export(
+    model,
+    dummy_input, 
+    "./model_files/recipes.onnx",
+    input_names=['inputs'],
+    output_names=['outputs'], 
+    export_params=True, 
+    opset_version=13,
+    dynamic_axes={'inputs' : {0 : 'batch_size', 1 : 'sequence_length'}, 'outputs' : {0 : 'batch_size', 1 : 'sequence_length'}}
+    )
